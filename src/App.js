9a2ef1c4ba1b3e328fc2823f0ebdc5e6f78bf9c6
Gabriel Kesslau <gabrielkesslau@gmail.com>
2020-04-16 14:12:04 -0300

    estrutura inicial

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import api from './services/api';
+
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
+import Header from './components/Header';
+
+function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    //projects.push(`Novo projeto ${Date.now()}`)
+
+    setProjects([... projects,`Novo projeto ${Date.now()}`]);
+
+    // console.log(projects);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <img src={backgroundImage} width={300} />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject} >Adicionar projeto</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const api = axios.create({
+  base_URL: 'http://localhost:3333'
+});
